e4856d70fdb9654a8b67e7732685d7edb1f9b966
Ulrond <131959864+Ulrond@users.noreply.github.com>
2023-11-15 09:48:05 +0000

    gh #6 - Corrected build script

--- template/ut_template/build.sh
+++ template/ut_template/build.sh
@@ -24,7 +24,11 @@ YELLOW="\e[1;33m"
 GREEN="\e[0;32m"
 NC="\e[39m"
 
-UT_PROJECT_MAJOR_VERSION="1."    # Change this to upgrade your UT-Core Major versions. Non ABI Changes 1.x.x are supported, between major revisions
+# The purpose of this script is on a clean environement, is to give you the latest binary compatible version for testing
+# When the major version changes in the ut-core, what that signals is that the testings will have to be upgraded to support that version
+# Therefore in that case it warns you but doesnt' chnage to that version, which could cause your tests to break.
+# Change this to upgrade your UT-Core Major versions. Non ABI Changes 1.x.x are supported, between major revisions
+UT_PROJECT_MAJOR_VERSION="1."  
 
 # Clone the Unit Test Requirements
 TEST_REPO=git@github.com:rdkcentral/ut-core.git
@@ -32,17 +36,17 @@ TEST_REPO=git@github.com:rdkcentral/ut-core.git
 # This function checks the latest version of UT core and recommends an upgrade if reuqired
 function check_next_revision()
 {
-    pushd ./ut-core 2&>/dev/null
-    # Set default UT_PROJECT_VERSION to next revision
-    if [ -v ${UT_PROJECT_VERSION} ]; then
-        UT_PROJECT_VERSION=$(git tag | grep ${UT_PROJECT_MAJOR_VERSION} | sort -r | head -n1)
+    pushd ./ut-core
+    # Set default UT_CORE_PROJECT_VERSION to next revision, if it's set then we don't need to tell you again
+    if [ -v ${UT_CORE_PROJECT_VERSION} ]; then
+        UT_CORE_PROJECT_VERSION=$(git tag | grep ${UT_PROJECT_MAJOR_VERSION} | sort -r | head -n1)
         UT_NEXT_VERSION=$(git tag | sort -r | head -n1)
-        echo -e ${YELLOW}ut-core version selected:[${UT_PROJECT_VERSION}]${NC}
-        if [ "${UT_NEXT_VERSION}" != "${UT_PROJECT_VERSION}" ]; then
+        echo -e ${YELLOW}ut-core version selected:[${UT_CORE_PROJECT_VERSION}]${NC}
+        if [ "${UT_NEXT_VERSION}" != "${UT_CORE_PROJECT_VERSION}" ]; then
             echo -e ${RED}--- New Version of ut-core released [${UT_NEXT_VERSION}] consider upgrading ---${NC}
         fi
     fi
-    popd 2&>/dev/null
+    popd
 }
 
 # Check if the common document configuration is present, if not clone it
@@ -55,7 +59,7 @@ else
     git clone ${TEST_REPO} ut-core
     check_next_revision
     cd ./ut-core
-    git checkout ${UT_PROJECT_VERSION}
+    git checkout ${UT_CORE_PROJECT_VERSION}
     ./build.sh
     cd ..
     ./${0} $@
