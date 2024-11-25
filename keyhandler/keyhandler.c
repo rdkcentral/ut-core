67e0443fad9367e9e0c2c2067aa09a5819e7becd
kanjoe24 <165808281+kanjoe24@users.noreply.github.com>
2024-11-25 14:09:24 +0000

    Fix gh#146 : Fixed review comments and code clean up

--- keyhandler/keyhandler.c
+++ keyhandler/keyhandler.c
@@ -21,20 +21,17 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
-#include <unistd.h>  // For write(), close(), and sleep()
 #include <fcntl.h>   // For open(), O_WRONLY, O_NONBLOCK
 #include <string.h>
 
 #include <ut_kvp_profile.h>
 #include <linux/uinput.h>
 #include <ut_log.h>
-#include <ut_kvp.h>
 
 void print_usage(const char *program_name)
 {
-    printf("Usage: %s -p <profile_file> -t <event_type> -c <event_code> -v <value>\n", program_name);
-    printf("OR if keys are included in the profile yaml file then\n");
-    printf("Usage: %s -p <profile_file>\n", program_name);
+    printf("\nUsage: %s -p <profile_file> -t <event_type> -c <event_code> -v <value>\n", program_name);
+    printf("More Info : https://github.com/rdkcentral/ut-core/discussions/148\n");
 }
 
 void emit(int fd, int type, int code, int val)
@@ -57,12 +54,12 @@ void emit(int fd, int type, int code, int val)
     write(fd, &ie, sizeof(ie));
 }
 
-int get_code(ut_kvp_instance_t *pInstance, char *code)
+int get_code(ut_kvp_instance_t *pInstance, char *type, char *code)
 {
     char key_code_str[256];
     int key_code;
 
-    snprintf(key_code_str, sizeof(key_code_str), "%s%s", "key_codes/", code);
+    snprintf(key_code_str, sizeof(key_code_str), "%s%s%s", type, "/", code);
     UT_LOG( "key_code_str=[%s]", key_code_str );
     key_code = ut_kvp_getUInt32Field( pInstance, key_code_str );
 
@@ -75,7 +72,7 @@ int get_type(ut_kvp_instance_t *pInstance, char *type)
     char event_type_str[256];
     int event_type_code;
 
-    snprintf(event_type_str, sizeof(event_type_str), "%s%s", "event_types/", type);
+    snprintf(event_type_str, sizeof(event_type_str), "%s%s", "EVENT_TYPE/", type);
     UT_LOG("event_type_str=[%s]", event_type_str);
     event_type_code = ut_kvp_getUInt32Field(pInstance, event_type_str);
 
@@ -106,16 +103,22 @@ int main(int argc, char *argv[])
     char *type = NULL;
     char *code = NULL;
     int value;
-    int list_count;
     ut_kvp_status_t status;
     ut_kvp_instance_t *pInstance;
-    //char result_code[UT_KVP_MAX_ELEMENT_SIZE]={-1};
     struct uinput_setup usetup;
-    
-    //char* kvpData;
-    
 
     // Parse command-line arguments
+    /*
+    •	-p <profile_file>
+        Path to the profile YAML file defining event types and key mappings. Required.
+    •	-t <event_type>
+        Type of the event (e.g., EV_KEY). Required.
+    •	-c <event_code>
+        Event code to emit (e.g., KEY_A). Required.
+    •	-v <value>
+        Event value to emit. Required.
+        •	1, 0 etc
+    */
     while ((opt = getopt(argc, argv, "p:t:c:v:")) != -1)
     {
         switch (opt)
@@ -144,22 +147,19 @@ int main(int argc, char *argv[])
     }
 
     // Validate required arguments
-    if (profile_file == NULL ) {
-        fprintf(stderr, "Error: Missing required arguments.\n");
+    if (profile_file == NULL)
+    {
+        UT_LOG_ERROR("Error: Missing required arguments.");
         print_usage(argv[0]);
         return 1;
     }
 
     pInstance = ut_kvp_profile_getInstance();
-    ///UT_ASSERT( pInstance != NULL );
     if(pInstance == NULL)
     {
         UT_LOG_ERROR("perhaps platform profile was not passed using -p switch");
         return -1;
     }
-
-    list_count = ut_kvp_getListCount(pInstance, "remote/events");
-    UT_LOG_DEBUG("listcount = [%d]", list_count);
 
     //print_yaml(pInstance);
 
@@ -182,56 +182,21 @@ int main(int argc, char *argv[])
     ioctl(fd, UI_DEV_SETUP, &usetup);
     ioctl(fd, UI_DEV_CREATE);
 
-    /*
-     * On UI_DEV_CREATE the kernel will create the device node for this
-     * device. We are inserting a pause here so that userspace has time
-     * to detect, initialize the new device, and can start listening to
-     * the event, otherwise it will not notice the event we are about
-     * to send. This pause is only needed in our example code!
-     */
     sleep(1);
 
-    // for(int i = 0; i < list_count; i++)
-    // {
-        if (type == NULL || code == NULL || value == 0)
-        {
-            ut_kvp_status_t status;
-            char result_type[UT_KVP_MAX_ELEMENT_SIZE] = {0xff};
-            char result_code[UT_KVP_MAX_ELEMENT_SIZE] = {0xff};
-            status = ut_kvp_getStringField(pInstance, "remote/events/0/event/type", result_type, UT_KVP_MAX_ELEMENT_SIZE);
-            if (status == UT_KVP_STATUS_SUCCESS)
-            {
-                UT_LOG_DEBUG("result_kvp for type = [%s]", result_type);
-            }
-            type = result_type;
+    // Print the parsed arguments
+    UT_LOG_DEBUG("Profile file: %s\n", profile_file);
+    UT_LOG_DEBUG("type: %s\n", type);
+    UT_LOG_DEBUG("code: %s\n", code);
+    UT_LOG_DEBUG("value: %d\n", value);
 
-            status = ut_kvp_getStringField(pInstance, "remote/events/0/event/code", result_code, UT_KVP_MAX_ELEMENT_SIZE);
-            if (status == UT_KVP_STATUS_SUCCESS)
-            {
-                UT_LOG_DEBUG("result_kvp for code = [%s]", result_code);
-            }
-            code = result_code;
+    /* Key press, report the event, send key release, and report again */
+    emit(fd, get_type(pInstance, type), get_code(pInstance, type, code), value);
 
-            value = ut_kvp_getUInt32Field(pInstance, "remote/events/0/event/value");
-            UT_LOG_DEBUG("result for value = [%d]", value);
-        }
-
-        // Print the parsed arguments
-        UT_LOG_DEBUG("Profile file: %s\n", profile_file);
-        UT_LOG_DEBUG("type: %s\n", type);
-        UT_LOG_DEBUG("code: %s\n", code);
-        UT_LOG_DEBUG("value: %d\n", value);
-
-        /* Key press, report the event, send key release, and report again */
-        emit(fd, get_type(pInstance, type), get_code(pInstance, code), value);
-   // }
-
-    
-    emit(fd, get_type(pInstance, type), get_code(pInstance, code), 0);
-    /*
-     * Give userspace some time to read the events before we destroy the
-     * device with UI_DEV_DESTROY.
-     */
+    // /*
+    //  * Give userspace some time to read the events before we destroy the
+    //  * device with UI_DEV_DESTROY.
+    //  */
     sleep(1);
 
     ioctl(fd, UI_DEV_DESTROY);
