9f63cf0c7bd65fb1576d7b91a1b2bb9ec572a345
kanjoe24 <165808281+kanjoe24@users.noreply.github.com>
2024-12-06 14:26:39 +0000

    Fix gh #152 : Fixing the source code to have comments and adding README

--- tests/demo/demo_weak_vs_strong_library_based/libweak/weak.c
+++ tests/demo/demo_weak_vs_strong_library_based/libweak/weak.c
@@ -1,6 +1,20 @@
+/**
+ * @file weak_my_function.c
+ * @brief Contains the weak implementation of `my_function`.
+ *
+ * This file provides a weak definition of `my_function`. If a strong definition
+ * is linked into the binary, it will override this weak implementation. This function
+ * serves as a default behavior when no strong implementation is available.
+ */
+
 #include <stdio.h>
 
-// Weak definition of my_function
+/**
+ * @brief Weak implementation of the `my_function`.
+ *
+ * Prints a message to indicate that the weak implementation is being executed.
+ * If a strong implementation is linked, this function will be overridden.
+ */
 void __attribute__((weak)) my_function() {
     printf("This is the weak implementation of my_function.\n");
 }
