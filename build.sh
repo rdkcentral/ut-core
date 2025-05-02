186cdc26a58522d028fdeba12f783d2fc1d37905
KarthikeyanR470 <179679359+KarthikeyanR470@users.noreply.github.com>
2025-05-02 06:23:39 +0100

    gh #190 added comment

--- build.sh
+++ build.sh
@@ -144,6 +144,8 @@ if [[ ! -d "${GTEST_DIR}/googletest-${GTEST_VERSION}" && "${VARIANT}" == "CPP" ]
     fi
     mkdir -p ${GTEST_LIB_DIR}
     cd ${GTEST_LIB_DIR}
+
+    # Prefer system cmake if version is > 3.12
     if command -v cmake &> /dev/null && [ "$(cmake --version | head -n1 | awk '{print $3}')" \> "3.12" ]; then
         cmake "${GTEST_DIR}/googletest-${GTEST_VERSION}/"
     else
